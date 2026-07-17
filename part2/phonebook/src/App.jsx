923860f6feaaba008a85434ebaf0ad2d03d2e672
Haden Hiles <hi@hadenhiles.com>
2026-07-17 17:41:17 -0400

    Add phone number field to phonebook form (exercise 2.8)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1 },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,9 +17,10 @@ const App = () => {
       return
     }
 
-    const person = { name: newName, id: persons.length + 1 }
+    const person = { name: newName, number: newNumber, id: persons.length + 1 }
     setPersons(persons.concat(person))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -29,13 +31,16 @@ const App = () => {
           name: <input value={newName} onChange={e => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.id}>{person.name}</li>
+          <li key={person.id}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
