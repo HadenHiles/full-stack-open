605295a84a9b4777058c4eeebde5c3b3d5a5a6bc
Haden Hiles <hi@hadenhiles.com>
2026-07-18 10:22:02 -0400

    Extract backend communication into persons service module (exercise 2.13)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,9 +11,7 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    personService.getAll().then(initialPersons => setPersons(initialPersons))
   }, [])
 
   const addPerson = (event) => {
@@ -26,11 +24,10 @@ const App = () => {
       return
     }
 
-    const person = { name: newName, number: newNumber }
-    axios
-      .post('http://localhost:3001/persons', person)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create({ name: newName, number: newNumber })
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
       })
