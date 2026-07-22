5ce878142c69c376e4806901d9584c3dee6ba2d8
Haden Hiles <hi@hadenhiles.com>
2026-07-21 20:08:44 -0400

    Connect phonebook frontend to backend API (exercise 3.9)

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,24 +1,27 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+	return axios
+		.get(baseUrl)
+		.then(personResponse => personResponse.data)
 }
 
-const create = (newObject) => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
+const create = (newPerson) => {
+	return axios
+		.post(baseUrl, newPerson)
+		.then(personResponse => personResponse.data)
 }
 
-const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
+const update = (id, updatedPerson) => {
+	return axios
+		.put(`${baseUrl}/${id}`, updatedPerson)
+		.then(personResponse => personResponse.data)
 }
 
 const remove = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
+	return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default { getAll, create, update, remove }
