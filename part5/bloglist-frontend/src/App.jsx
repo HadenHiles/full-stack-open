f30e6885eaef2adfa55ba4eebadd271416f509fe
Haden Hiles <hi@hadenhiles.com>
2026-07-25 15:19:43 -0400

    Allow users to create blogs (exercise 5.3)

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(setBlogs)
@@ -43,6 +46,15 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogappUser')
     blogService.setToken(null)
     setUser(null)
+  }
+
+  const createBlog = async (event) => {
+    event.preventDefault()
+    const createdBlog = await blogService.create({ title, author, url })
+    setBlogs(blogs.concat(createdBlog))
+    setTitle('')
+    setAuthor('')
+    setUrl('')
   }
 
   if (!user) {
@@ -81,6 +93,22 @@ const App = () => {
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
+      <h2>create new</h2>
+      <form onSubmit={createBlog}>
+        <div>
+          title:
+          <input value={title} onChange={({ target }) => setTitle(target.value)} />
+        </div>
+        <div>
+          author:
+          <input value={author} onChange={({ target }) => setAuthor(target.value)} />
+        </div>
+        <div>
+          url:
+          <input value={url} onChange={({ target }) => setUrl(target.value)} />
+        </div>
+        <button type="submit">create</button>
+      </form>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
