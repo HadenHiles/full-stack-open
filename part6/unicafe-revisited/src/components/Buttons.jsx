94b364ab1fe216a83b996800b08ef42b39f4908d
Haden Hiles <hi@hadenhiles.com>
2026-07-29 15:39:27 -0400

    Implement Unicafe with Zustand (exercise 6.1)

---
+++ part6/unicafe-revisited/src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import { useFeedbackActions } from '../store'
+
+const Buttons = () => {
+	const { addGood, addNeutral, addBad } = useFeedbackActions()
+
+	return (
+		<div>
+			<h2>give feedback</h2>
+			<button onClick={addGood}>good</button>
+			<button onClick={addNeutral}>neutral</button>
+			<button onClick={addBad}>bad</button>
+		</div>
+	)
+}
+
+export default Buttons
---
+++ part6/unicafe-revisited/src/store.js
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+const useFeedbackStore = create(set => ({
+	good: 0,
+	neutral: 0,
+	bad: 0,
+	actions: {
+		addGood: () => set(state => ({ good: state.good + 1 })),
+		addNeutral: () => set(state => ({ neutral: state.neutral + 1 })),
+		addBad: () => set(state => ({ bad: state.bad + 1 })),
+	},
+}))
+
+export const useFeedback = () => useFeedbackStore(state => state)
+export const useFeedbackActions = () => useFeedbackStore(state => state.actions)
