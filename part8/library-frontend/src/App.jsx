a15a52b0bedff032e3d0876bfd1f7138a944af5a
Haden Hiles <hi@hadenhiles.com>
2026-08-06 13:38:51 -0400

    Add new book form with genre list (exercise 8.10)

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
+import AddBook from './components/AddBook'
 
-const VIEWS = { authors: 'authors', books: 'books' }
+const VIEWS = { authors: 'authors', books: 'books', add: 'add' }
 
 const App = () => {
 	const [activeView, setActiveView] = useState(VIEWS.authors)
@@ -12,9 +13,11 @@ const App = () => {
 			<nav>
 				<button onClick={() => setActiveView(VIEWS.authors)}>authors</button>
 				<button onClick={() => setActiveView(VIEWS.books)}>books</button>
+				<button onClick={() => setActiveView(VIEWS.add)}>add book</button>
 			</nav>
 			{activeView === VIEWS.authors && <Authors />}
 			{activeView === VIEWS.books && <Books />}
+			{activeView === VIEWS.add && <AddBook />}
 		</div>
 	)
 }
