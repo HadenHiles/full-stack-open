4489b0760a4be2969e2075cbe1bb3e299ed511ae
Haden Hiles <hi@hadenhiles.com>
2026-08-12 21:38:01 -0400

    sort out part9 config and data files

--- part9/bmi-exercises/eslint.config.mjs
+++ part9/bmi-exercises/eslint.config.mjs
@@ -2,20 +2,20 @@ import tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 
 export default [
-	{
-		files: ['**/*.ts'],
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				project: './tsconfig.json',
-			},
-		},
-		plugins: { '@typescript-eslint': tseslint },
-		rules: {
-			...tseslint.configs.recommended.rules,
-			'@typescript-eslint/no-explicit-any': 'error',
-			'@typescript-eslint/explicit-function-return-type': 'off',
-			'@typescript-eslint/explicit-module-boundary-types': 'off',
-		},
-	},
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+        plugins: { '@typescript-eslint': tseslint },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+        },
+    },
 ]
